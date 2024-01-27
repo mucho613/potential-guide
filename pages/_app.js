8c2047e877f404af77e233fe0441c4ebd91f094f
Daisuke Yamamura <mucho613@gmail.com>
2024-01-27 22:32:51 +0900

    wip

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import './global.css'
+
+import { Noto_Sans_JP } from 'next/font/google'
+
+const notoSansJP = Noto_Sans_JP({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <div className={notoSansJP.className}><Component {...pageProps} /></div>
+}
