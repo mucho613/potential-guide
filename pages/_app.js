98d4b4108b27a536801996afd758bbdff6670bed
Daisuke Yamamura <mucho613@gmail.com>
2025-01-13 22:17:55 +0900

    update: format

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import './global.css'
+import "./global.css";
 
-import { Noto_Sans_JP } from 'next/font/google'
+import { Noto_Sans_JP } from "next/font/google";
 
 const notoSansJP = Noto_Sans_JP({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-})
+	weight: "400",
+	subsets: ["latin"],
+	display: "swap",
+});
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <div className={notoSansJP.className}><Component {...pageProps} /></div>
+	return (
+		<div className={notoSansJP.className}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
